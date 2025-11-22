45890536cc88e92288713af8c2ef4f84b6fe85ff
gokay <gokayacikgoz1@gmail.com>
2025-11-22 15:15:47 +0300

    Automapper added

--- Microservice.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
+++ Microservice.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
@@ -1,4 +1,5 @@
 ﻿using Microservice.Catalog.Api.Features.Categories.Create;
+using Microservice.Catalog.Api.Features.Categories.GetAll;
 
 namespace Microservice.Catalog.Api.Features.Categories
 {
@@ -6,7 +7,9 @@ namespace Microservice.Catalog.Api.Features.Categories
     {
         public static void AddCategoryGroupEndpointExt(this WebApplication app)
         {
-            app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint();
+            app.MapGroup("api/categories")
+                .CreateCategoryGroupItemEndpoint()
+                .GetAllCategoryGroupItemEndpoint();
         }
     }
 }
---
+++ Microservice.Catalog.Api/Features/Categories/GetAll/GetAllCategoryEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microservice.Catalog.Api.Features.Categories.Create;
+using Microservice.Catalog.Api.Features.Categories.Dtos;
+using Microservice.Catalog.Api.Repositories;
+using Microservice.Shared;
+using Microservice.Shared.Extensions;
+using Microservice.Shared.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.Catalog.Api.Features.Categories.GetAll
+{
+
+    public class GetAllCategoryQuery : IRequest<ServiceResult<List<CategoryDto>>>;
+
+    public class GetAllCategoryQueryHandler(AppDbContext context, IMapper mapper) : IRequestHandler<GetAllCategoryQuery, ServiceResult<List<CategoryDto>>>
+    {
+        public async Task<ServiceResult<List<CategoryDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await context.Categories.ToListAsync(cancellationToken);
+            var categoriesAsDto = mapper.Map<List<CategoryDto>>(categories);
+
+            return ServiceResult<List<CategoryDto>>.SuccessAsOkey(categoriesAsDto);
+        }
+    }
+
+
+
+
+    public static class GetAllCategoryEndpoint
+    {
+        public static RouteGroupBuilder GetAllCategoryGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("/", async (IMediator mediator) => (await mediator.Send(new GetAllCategoryQuery())).ToGenericResult());
+
+            return group;
+        }
+    }
+}
